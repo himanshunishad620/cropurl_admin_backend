2bf71f440e801e22a34902c5cd2667da42655e91
himanshu_nishad <himanshunishad620@gmail.com>
2026-08-26 18:09:39 +0530

    sameSite changed

--- controllers/authController.js
+++ controllers/authController.js
@@ -238,7 +238,7 @@ const doLogin = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 4 * 24 * 60 * 60 * 1000,
     });
 
