0c7bd07e4029cf35096a332c46649657682beefe
himanshu_nishad <himanshunishad620@gmail.com>
2026-08-26 17:27:40 +0530

    root api added

--- app/index.js
+++ app/index.js
@@ -2,6 +2,8 @@ const { app } = require("../app");
 const connectDB = require("../config/db");
 connectDB();
 
+app.get("/", (req, res) => res.send("himanshu"));
+
 module.exports = app;
 // const PORT = 5000;
 
