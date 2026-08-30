dc7c01fdd834a0d628b7caba2f549b2dfed03dc5
himanshu_nishad <himanshunishad620@gmail.com>
2026-08-30 19:48:04 +0530

    .env removed

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", process.env.CLIENT_URL],
     credentials: true,
   }),
 );
