1a0c66bf44514c851d80e2f7c468c1fe74682208
himanshu_nishad <himanshunishad620@gmail.com>
2026-09-06 09:10:45 +0530

    origin added

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: ["http://localhost:5173", process.env.CLIENT_URL],
     credentials: true,
   }),
 );
